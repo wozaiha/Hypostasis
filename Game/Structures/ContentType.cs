fc2f837e0051a2c2ef7e5971a5f8f1bafce20297
Tsubasahane <wozaiha@gmail.com>
2024-10-24 10:27:04 +0800

    Create ContentType.cs

---
+++ Game/Structures/ContentType.cs
@@ -0,0 +1,12 @@
+﻿namespace Hypostasis.Game.Structures;
+
+public enum ContentType
+{
+    迷宫挑战 = 2,
+    讨伐歼灭战 = 4,
+    大型任务 = 5,
+    绝境战 = 28,
+    特殊迷宫探索 = 30,
+    //对战 = 6,
+    //金碟游乐场 = 19,
+}
